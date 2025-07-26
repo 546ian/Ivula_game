6755e2f79cb73db0c55826fb7338592706721395
546ian <ianwaweru38@gmail.com>
2025-07-26 09:52:29 +0300

    Add audio system, sound assets, and DOTween plugin
    Introduced new scripts for audio management (SoundManager, MusicManager, SoundLibrary, MusicLibrary, CrossFade, SceneTransition, LevelManager). Added sound effect and music meta files, Unity audio mixer, and theme song assets. Integrated DOTween plugin for tweening support. Renamed 'Main menu.unity' to 'MainMenu.unity' and updated build settings accordingly.

--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -1,22 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
-public class mainMenu : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
+    public GameObject music;
+
+    public AudioMixer audioMixer;
+
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+    public void Start()
+    {
+        LoadVolume();  
+        // Play the main menu music
+        MusicManager.Instance.PlayMusic("Main Menu");
+    }
+
     public void PlayGame()
     {
         // Load the game scene
-        SceneManager.LoadSceneAsync(1);
+        LevelManager.Instance.LoadScene("Gameplay", "CrossFade");
+
+        music.SetActive(false);
     }
 
     public void QuitGame()
     {
         // Quit the application
         Application.Quit();
-        
+
         // If running in the editor, log a message
-        #if UNITY_EDITOR
+#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
-        #endif
+#endif
+    }
+
+    public void UpdateMusicVolume(float volume)
+    {
+        // Update the music volume in the audio mixer
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void UpdateSFXVolume(float volume)
+    {
+        // Update the sound effects volume in the audio mixer
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+
+    public void SaveVolume()
+    {
+        audioMixer.GetFloat("MusicVolume", out float musicVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+
+        audioMixer.GetFloat("SFXVolume", out float sfxVolume);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+    }
+
+    public void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume"); 
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
     }
 }
